7d42f7ce78ed70c17eea2b724c805874dd187531
Jeremy Nelson <jeremy.nelson@coloradocollege.edu>
2011-06-24 09:09:02 -0600

    First commit for django version of CCETD

---
+++ etd/models.py
@@ -0,0 +1,28 @@
+"""
+ models.py - Creates electronic thesis and dataset content object for 
+ ingestion into a Fedora repository.
+
+
+ (c) 2011 Colorado College
+
+"""
+from django.db import models
+from eulfedora.models import DigitalObject,FileDatastream
+import settings
+
+class ThesisDatasetObject(DigitalObject):
+    """
+    `ThesisDatasetObject` models the Electronic Thesis and Dataset datastreams
+    for ingestion into a Fedora Repository.
+    """
+    ETD_CONTENT_MODEL = 'info:fedora/coalliance:%s' % settings.FEDORA_ETDCMODEL
+    CONTENT_MODELS = [ ETD_CONTENT_MODEL ]
+    dataset = FileDatastream("FILE",
+                             "Dataset datastream",
+                             defaults={'versionable': True })
+    thesis = FileDatastream("FILE",
+                            "Thesis datastream",
+                            defaults={'versionable': True,
+                                      'mimetype': 'application/pdf'})
+    
+
